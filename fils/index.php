56f2d42896e3c5e51d147b00524242f3ce5b2152
Mlxxs <mzh9966@foxmail.com>
2020-04-06 16:17:11 +0800

    123

--- fils/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $url = 'https://cdn.jsdelivr.net/gh/mlxxs/photos/background/'.(string)rand(1,600).'.jpg';
-    header('Content-Type: image/png');
-	echo file_get_contents($url);
-?>
---
+++ wsy/index.js
@@ -0,0 +1,6 @@
+function img(){
+var url = "https://cdn.jsdelivr.net/gh/mlxxs/photos/wsy/";
+url=url+string(rand(1,51));
+url=url+".jpg";
+return url;
+}
